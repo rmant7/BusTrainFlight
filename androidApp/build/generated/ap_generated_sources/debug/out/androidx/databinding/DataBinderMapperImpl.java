fb076802cbd739383ee8a6eebdb82edf71d46abf
Alyona Chebanyk <alyonachebanyk@gmail.com>
2023-06-28 22:43:21 +0300

    gitignore

--- androidApp/build/generated/ap_generated_sources/debug/out/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new ru.z8.louttsev.bustrainflightmobile.androidApp.DataBinderMapperImpl());
-  }
-}
